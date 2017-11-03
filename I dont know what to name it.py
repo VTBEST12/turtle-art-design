70428a9cb9f2891300f4b6da24c5a08eef7eedeb
VTBEST12 <33353775+VTBEST12@users.noreply.github.com>
2017-11-03 11:45:57 -0400

    Add files via upload

---
+++ I dont know what to name it.py
@@ -0,0 +1,173 @@
+import turtle
+turtle.colormode(255)
+bob=turtle.Turtle()
+turtle.bgcolor("black")
+bob.width(5)
+bob.speed(500)
+c = (217,98,175)
+for times in range(40):
+    c = ("lime")
+    bob.color(c)
+    bob.circle(100)
+    bob.right(7000)
+  
+for times in range(30):
+    c = ("orange")
+    bob.color(c)
+    bob.circle(100)
+    bob.right(678)
+
+for times in range(20):
+    c = ("magenta")
+    bob.color(c)
+    bob.circle(100)
+    bob.right(461)
+    
+for times in range(10):
+    c = ("yellow")
+    bob.color(c)
+    bob.circle(100)
+    bob.right(150)
+
+for times in range(7):
+    c = ("red")
+    bob.color(c)
+    bob.circle(100)
+    bob.right(90)
+
+bob.penup()
+bob.goto(-600,300)
+bob.pendown()
+
+for times in range(40):
+    c = ("sky blue")
+    bob.color(c)
+    bob.circle(100)
+    bob.right(7000)
+  
+for times in range(30):
+    c = ("green")
+    bob.color(c)
+    bob.circle(100)
+    bob.right(678)
+
+for times in range(20):
+    c = ("pink")
+    bob.color(c)
+    bob.circle(100)
+    bob.right(461)
+    
+for times in range(10):
+    c = ("white")
+    bob.color(c)
+    bob.circle(100)
+    bob.right(150)
+
+for times in range(7):
+    c = ("brown")
+    bob.color(c)
+    bob.circle(100)
+    bob.right(90)
+
+bob.penup()
+bob.goto(600,-300)
+bob.pendown()
+
+for times in range(40):
+    c = (119,221,231)
+    bob.color(c)
+    bob.circle(100)
+    bob.right(7000)
+  
+for times in range(30):
+    c = (253,252,116)
+    bob.color(c)
+    bob.circle(100)
+    bob.right(678)
+
+for times in range(20):
+    c = (227,37,107)
+    bob.color(c)
+    bob.circle(100)
+    bob.right(461)
+    
+for times in range(10):
+    c = (116,66,200)
+    bob.color(c)
+    bob.circle(100)
+    bob.right(150)
+
+for times in range(7):
+    c = (252,108,133)
+    bob.color(c)
+    bob.circle(100)
+    bob.right(90)
+    
+bob.penup()
+bob.goto(-600,-300)
+bob.pendown()
+         
+for times in range(40):
+    c = (116,66,200)
+    bob.color(c)
+    bob.circle(100)
+    bob.right(7000)
+  
+for times in range(30):
+    c = (227,37,107)
+    bob.color(c)
+    bob.circle(100)
+    bob.right(678)
+
+for times in range(20):
+    c = (159,226,191)
+    bob.color(c)
+    bob.circle(100)
+    bob.right(461)
+    
+for times in range(10):
+    c = (255,110,74)
+    bob.color(c)
+    bob.circle(100)
+    bob.right(150)
+
+for times in range(7):
+    c = (252,108,133)
+    bob.color(c)
+    bob.circle(100)
+    bob.right(90)
+
+bob.penup()
+bob.goto(600,300)
+bob.pendown()
+
+for times in range(40):
+    c = (237,237,237)
+    bob.color(c)
+    bob.circle(100)
+    bob.right(7000)
+  
+for times in range(30):
+    c = (255,29,206)
+    bob.color(c)
+    bob.circle(100)
+    bob.right(678)
+
+for times in range(20):
+    c = (238,32,77)
+    bob.color(c)
+    bob.circle(100)
+    bob.right(461)
+    
+for times in range(10):
+    c = (253,252,116)
+    bob.color(c)
+    bob.circle(100)
+    bob.right(150)
+
+for times in range(7):
+    c = (28,211,162)
+    bob.color(c)
+    bob.circle(100)
+    bob.right(90)
+
